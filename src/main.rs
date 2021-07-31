a5d4e5a01102ba91a7632e2efcd037b520ad6c37
Marty Jones <murtin.jones@gmail.com>
2021-07-31 09:06:20 -0400

    Add custom handlebars helper for checking array contains

--- src/main.rs
+++ src/main.rs
@@ -291,6 +291,44 @@ fn main() {
             )
             
         )
-        .attach(Template::fairing())
-        .launch();
+        .attach(Template::custom(|engines| {
+            engines.handlebars.register_helper("contains", Box::new(contains));
+        }))
+}
+
+use rocket_dyn_templates::handlebars::{Handlebars, HelperDef, RenderContext, Helper, Context, JsonRender, HelperResult, Output, RenderError};
+
+fn contains<'reg, 'rc>(
+    h: &Helper<'reg, 'rc>,
+    r: &'reg Handlebars,
+    ctx: &'rc Context,
+    rc: &mut RenderContext<'reg, 'rc>,
+    out: &mut dyn Output,
+) -> HelperResult {
+    // param 0 should be an array
+    // param 1 should be a string
+    // param 0 should contain param 1
+    let array = h.param(0);
+    if array.is_none() {
+        return Err(RenderError::new("No array given"));
+    }
+    let array = h.param(0).unwrap().value().as_array();
+    if array.is_none() {
+        return Err(RenderError::new("First param should be an array"));
+    }
+    let array = array.unwrap();
+    let needle = h.param(1);
+    if needle.is_none() {
+        return Err(RenderError::new("No needle given"));
+    }
+    let needle = needle.unwrap().value();
+    if array.contains(needle) {
+        return h.template()
+        .map(|t| t.render(r, ctx, rc, out))
+        .unwrap_or(Ok(()));
+    } else {
+        return h.inverse()
+        .map(|t| t.render(r, ctx, rc, out))
+        .unwrap_or(Ok(()));
+    }
 }
